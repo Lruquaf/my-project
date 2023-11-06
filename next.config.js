e3cd7dceabe048c37c75c88a635d265a16e3f413
Lruquaf <yavuzselimtuncer02@gmail.com>
2023-11-06 14:21:04 +0300

    fixed2

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
     reactStrictMode: true,
     basePath: "",
     pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+    webpack: (config) => {
+        config.resolve.fallback = {
+            fs: false,
+            path: false,
+            stream: false,
+            constants: false,
+        };
+        return config;
+    },
 };
 
 module.exports = nextConfig;
