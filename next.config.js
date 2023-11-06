e214441d7e0a509ca637010bf2159ac7db5c7882
Lruquaf <yavuzselimtuncer02@gmail.com>
2023-11-06 14:13:28 +0300

    fixed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: "",
+    reactStrictMode: true,
+    basePath: "",
+    pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
 };
 
 module.exports = nextConfig;
