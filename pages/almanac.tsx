9e83c50a7ee65005e4afefab625d6b7e83f8d5d3
Lruquaf <yavuzselimtuncer02@gmail.com>
2023-11-06 17:53:02 +0300

    fixed5

--- pages/almanac.tsx
+++ pages/almanac.tsx
@@ -1,9 +1,9 @@
-import { GetStaticProps, NextPage } from "next";
+import { NextPage } from "next";
 import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import Modal from "./Modal";
 import { MediaRenderer } from "@thirdweb-dev/react";
-import { textContentList } from "./data";
+// import { textContentList } from "./data";
 
 // interface AlmanacProps {
 //     textContentList: TextContent[];
@@ -20,6 +20,28 @@ const Almanac: NextPage = () => {
     const [expandedCardId, setExpandedCardId] = useState<string | null>(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [expandedCard, setExpandedCard] = useState<TextContent | null>(null);
+
+    const textContentList = [
+        {
+            id: "0",
+            name: "Alexios",
+            description:
+                "Nikephoros II Phokas reigned as Byzantine Emperor between 963 and 969. When he ascended to the throne in 969, he took back Antioch, which was occupied by the Arabs, and conquered Syria. He also captured the regions of Northern Mesopotamia and Cilicia. Despite his successes abroad, he was known as a very harsh and authoritarian leader in domestic politics. He also sought to centralize the Byzantine Empire's administration of the conquered regions. Nikephoros II Phokas was assassinated and died in 969. Nikephoros II Phokas was an important figure in the historical process of the Byzantine Empire, especially remembered for his victories in the wars with the Arabs.",
+            image: "ipfs://Qmf4akzSyjzEtBq1FbWRVmKAvUoA5pYU47cmpx4SDnKNRQ",
+        },
+        {
+            id: "1",
+            name: "Attila",
+            description: "The famous khan of Hunnic Empire",
+            image: "ipfs://QmY5yfkejsByNU8epmU3ohxVK8rnfw47poZ9mEzia6b9Lj",
+        },
+        {
+            id: "2",
+            name: "Belisarius",
+            description: "The supreme general of Byzantine Empire",
+            image: "ipfs://QmU8Rf6Lzz7fPF76B6WRRyeYVd1KLNKFvi9DRDvFnbnYut",
+        },
+    ];
 
     const handleCardClick = (id: string | null) => {
         if (expandedCardId === id) {
@@ -75,8 +97,8 @@ const Almanac: NextPage = () => {
                                 <MediaRenderer
                                     className={styles.general}
                                     src={expandedCard?.image}
-                                    width="160px"
-                                    height="160px"
+                                    width="250px"
+                                    height="250px"
                                 />
                                 <p className={styles.generalDescription}>
                                     {expandedCard?.description}
